1a49309f1a8aa67eec3d06fe6359b6668091d4ab
Kelly Shen <kellyshenfrog@gmail.com>
2021-03-07 03:11:28 -0600

    questions

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -8,15 +8,99 @@ export default class MyForm extends React.Component {
       {
         'tag': 'input',
         'type': 'text',
-        'name': 'firstname',
-        'cf-questions': 'What is your firstname?'
+        'name': 'name',
+        'cf-questions': 'May I know your name?',
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'yes',
+        'cf-questions': 'Do you mind if I ask some questions to learn more about you?',
+        'cf-label': 'Nope',
+        'value': 'yes',
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'yes',
+        'cf-label': 'Go Ahead',
+        'value': 'yes',
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'pregnancy',
+        'cf-questions': `Great! Let's get started. What stage of pregnancy are you in?`,
+        'cf-label': 'First Trimester (1-12 weeks)',
+        'value': 'first',
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'pregnancy',
+        'cf-label': 'Second trimester (13-27 weeks)',
+        'value': 'second',
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'pregnancy',
+        'cf-label': 'Third trimester (28-40) weeks',
+        'value': 'second',
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'pregnancy',
+        'cf-label': 'Postpartum',
+        'value': 'postpartum',
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'mood',
+        'cf-questions': `I see. How are you feeling today?`,
+        'cf-label': 'Good',
+        'cf-image': "https://openmoji.org/data/color/svg/1F60C.svg",
+        'tabindex': '1',
+        'value': 'good'
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'mood',
+        'cf-questions': `I see. How are you feeling today?`,
+        'cf-label': 'Meh',
+        'cf-image': "https://openmoji.org/data/color/svg/1F610.svg",
+        'tabindex': '2',
+        'value': 'meh'
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'mood',
+        'cf-questions': `I see. How are you feeling today?`,
+        'cf-label': 'Sad',
+        'cf-image': "https://openmoji.org/data/color/svg/1F641.svg",
+        'tabindex': '3',
+        'value': 'sad'
+      },
+      {
+        'tag': 'input',
+        'type': 'radio',
+        'name': 'mood',
+        'cf-questions': `I see. How are you feeling today?`,
+        'cf-label': 'Anxious',
+        'cf-image': "https://openmoji.org/data/color/svg/1F630.svg",
+        'tabindex': '4',
+        'value': 'anxious'
       },
       {
         'tag': 'input',
         'type': 'text',
-        'name': 'lastname',
-        'cf-questions': 'What is your lastname?'
-      }
+        'name': 'reason',
+        'cf-questions': 'Could you tell me why you feel that way?',
+      },
     ];
     
     this.submitCallback = this.submitCallback.bind(this);
@@ -32,13 +116,14 @@ export default class MyForm extends React.Component {
       },
       tags: this.formFields
     });
+    this.cf.addRobotChatResponse('Hi There! Welcome to our website! My name is Happi and I will be helping you today.');
     this.elem.appendChild(this.cf.el);
   }
   
   submitCallback() {
     var formDataSerialized = this.cf.getFormData(true);
     console.log("Formdata, obj:", formDataSerialized);
-    this.cf.addRobotChatResponse("Y")
+    this.cf.addRobotChatResponse("Thanks for telling me! I have some helpful results below for you.")
   }
   
   render() {
